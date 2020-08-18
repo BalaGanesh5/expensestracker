b7a66ab96518c0f0d98b75ff0beb7b1c18a03462
BalaGanesh5 <58981994+BalaGanesh5@users.noreply.github.com>
2020-08-18 20:02:04 +0530

    Defined Components
    Definend all Components, have to update the functionality

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const AddTransaction = () => {
+
+    const [text, setText] = useState([])
+    const [amount, setAmount] = useState([])
+    return (
+        <>
+            <h3>Add New Transaction</h3>
+            <form >
+                <div className="form-control">
+                    <lable htmlFor="text"> Text</lable>
+                    <input type="text"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        placeholder="Enter text here . . ." />
+                </div>
+                <div className="form-control">
+                    <lable htmlFor="amount">
+                        Amount <br />
+                        (negative-expense, positive-income)
+                    </lable>
+                    <input type="number"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                        placeholder="Enter Amount here . . ." />
+                </div>
+                <button className="button">Add Transaction</button>
+            </form>
+        </>
+    )
+}
+
+export default AddTransaction
---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Balance = () => {
+    return (
+        <>
+            <h4>Your Balance is:</h4>
+            <h1>50.00</h1>
+        </>
+    )
+}
+
+export default Balance
