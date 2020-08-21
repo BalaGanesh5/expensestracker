a0972a0bdc45698efec5f96f92fecb4662b8293b
BalaGanesh5 <balaganesh405@gmail.com>
2020-08-21 23:26:41 +0530

    Updates

---
+++ precache-manifest.93d73ae397f0ec68f7f02bdb1d935a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0155a32087735d4be6a870eaae96a5",
+    "url": "/expensestracker/index.html"
+  },
+  {
+    "revision": "b8e7c000035a8746828e",
+    "url": "/expensestracker/static/css/main.24f06dc3.chunk.css"
+  },
+  {
+    "revision": "f79b1d81a4547064afcb",
+    "url": "/expensestracker/static/js/2.348e84aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expensestracker/static/js/2.348e84aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e7c000035a8746828e",
+    "url": "/expensestracker/static/js/main.cded2ed2.chunk.js"
+  },
+  {
+    "revision": "b3c5270d4c6c1ba92242",
+    "url": "/expensestracker/static/js/runtime-main.8018b005.js"
+  }
+]);
